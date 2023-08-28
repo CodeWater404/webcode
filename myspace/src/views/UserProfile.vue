36fa6ed1b07f5535a10beed789967e3833b87700
codewater404 <1746559397@qq.com>
2023-08-29 00:08:11 +0800

    add views: login , register , notfound , userprofile

---
+++ myspace/src/views/UserProfile.vue
@@ -0,0 +1,17 @@
+<template>
+    <ContentBase>
+        用户动态
+    </ContentBase>
+</template>
+  
+<script>
+import ContentBase from "../components/ContentBase.vue";
+export default {
+    name: 'UserProfileView',
+    components: {
+        ContentBase,
+    }
+}
+</script>
+  
+<style scoped></style>
