963893190a0f05e4cb0ad2f5fda68952201d953f
codewater404 <1746559397@qq.com>
2023-08-30 00:06:55 +0800

    add routers

--- myspace/src/components/NavBar.vue
+++ myspace/src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="#">Myspace</a>
+            <!-- 这里采用router那里的路由，这样前端就能依靠点击进入，而不是直接修改地址栏。 -->
+            <!-- 主要是router-link和：to这两个绑定实现 -->
+            <router-link class="navbar-brand" :to="{ name: 'home' }">Myspace</router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -9,21 +11,21 @@
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="#">首页</a>
+                        <router-link class="nav-link" aria-current="page" :to="{ name: 'home' }">首页</router-link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">好友列表</a>
+                        <router-link class="nav-link" :to="{ name: 'userlist' }">好友列表</router-link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">用户动态</a>
+                        <router-link class="nav-link" :to="{ name: 'userprofile' }">用户动态</router-link>
                     </li>
                 </ul>
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">登录</a>
+                        <router-link class="nav-link" :to="{ name: 'login' }">登录</router-link>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">注册</a>
+                        <router-link class="nav-link" :to="{ name: 'register' }">注册</router-link>
                     </li>
                 </ul>
 
--- myspace/src/router/index.js
+++ myspace/src/router/index.js
@@ -2,13 +2,44 @@
 // import { createRouter, createWebHashHistory } from 'vue-router'
 // 如果不需要带有#，去掉hash
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
+import LoginView from '../views/LoginView.vue';
+import NotFoundView from '../views/NotFoundView.vue';
+import RegisterView from '../views/RegisterView.vue';
+import UserListView from '../views/UserListView.vue';
+import UserProfileView from '../views/UserProfileView.vue';
 
+//路由，就是地址那里的地址
 const routes = [
   {
-    path: '/',
+    path: '/home',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/notfound',
+    name: 'notfound',
+    component: NotFoundView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
+  {
+    path: '/userlist',
+    name: 'userlist',
+    component: UserListView
+  },
+  {
+    path: '/userprofile',
+    name: 'userprofile',
+    component: UserProfileView
   },
 ]
 
