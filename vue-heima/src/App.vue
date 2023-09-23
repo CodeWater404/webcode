a37da5c7eb961db73c265bcb6447886828fe84ee
codewater404 <1746559397@qq.com>
2023-09-24 00:25:24 +0800

    vue-heima: computed

--- vue-heima/src/App.vue
+++ vue-heima/src/App.vue
@@ -1,26 +1,24 @@
+<!-- computed -->
 <script setup>
-import { reactive } from 'vue';
-import { ref } from 'vue';
+import { ref } from 'vue'
+import { computed } from 'vue'
 
-// reactive接收对象类型参数(不能是简单类型的！)，返回响应式对象
-const state = reactive({
-  count: 0
+const list = ref([1, 2, 3, 4, 5, 6, 7, 8])
+
+const computedList = computed(() => {
+    return list.value.filter(item => item > 3)
 })
 
-const setCount = () => {
-  state.count++
-}
-
-// ref接收基本类型和对象类型参数，返回响应式对象
-const state2 = ref(0)
-
-const setCount2 = () => {
-  //脚本区域修改ref产生的响应式对象数据必须通过.value属性！！！！
-  state2.value++
-}
+setTimeout(() => {
+    list.value.push(9, 10)
+}, 3000)
 </script>
 
 <template>
-  <div><button @click="setCount">count1:{{ state.count }}</button></div>
-  <div><button @click="setCount2">count2:{{ state2 }}</button></div>
+    <div>
+        原始响应式数据：{{ list }}
+    </div>
+    <div>
+        计算属性数据：{{ computedList }}
+    </div>
 </template>
---
+++ vue-heima/src/AppExample_2.vue
@@ -0,0 +1,28 @@
+<!-- ref、reactive -->
+
+<script setup>
+import { reactive } from 'vue';
+import { ref } from 'vue';
+
+// reactive接收对象类型参数(不能是简单类型的！)，返回响应式对象
+const state = reactive({
+  count: 0
+})
+
+const setCount = () => {
+  state.count++
+}
+
+// ref接收基本类型和对象类型参数，返回响应式对象
+const state2 = ref(0)
+
+const setCount2 = () => {
+  //脚本区域修改ref产生的响应式对象数据必须通过.value属性！！！！
+  state2.value++
+}
+</script>
+
+<template>
+  <div><button @click="setCount">count1:{{ state.count }}</button></div>
+  <div><button @click="setCount2">count2:{{ state2 }}</button></div>
+</template>
