b5b999551ce26de155e8400dd0adcd5104d85805
codewater404 <1746559397@qq.com>
2023-09-24 16:33:56 +0800

    vue-heima: template reference

---
+++ vue-heima/src/AppTest.vue
@@ -0,0 +1,26 @@
+<!-- 模板引用的跳念
+通过ref标识获取真实的dom对象或者组件实例对象
+ -->
+<script setup>
+import { onMounted, ref } from 'vue'
+import TestCom from './AppTestCom.vue'
+
+// 用ref函数，获取ref对象
+const h1Ref = ref(null)
+const comRef = ref(null)
+
+// 组件挂载完毕之后才能获取，用onMounted函数确保这一点
+onMounted(() => {
+    // 打印获取到的dom对象
+    console.log("h1ref:", h1Ref.value)
+    // 获取到的组件对象，如果需要内部的属性和函数，需要在组件内使用defineExpose暴露出来
+    console.log("comRef:", comRef.value)
+})
+
+</script>
+
+<template>
+    <!-- 通过ref标识获取真实的dom对象或者组件实例对象,绑定到变量h1Ref、comRef上去 -->
+    <h1 ref="h1Ref">我是dom标签h1</h1>
+    <TestCom ref="comRef" />
+</template>
---
+++ vue-heima/src/AppTestCom.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const name = ref('test name')
+const setName = () => {
+    name.value = 'new test name'
+}
+
+
+// defineExpose()
+// 默认情况下在<script setup>语法糖下组件内部的属性和方法是不开放给父组件访问的，可以通过defineExpose编译宏指定哪些属性和
+// 方法允许访问
+defineExpose({
+    name,
+    setName,
+
+})
+</script>
+
+<template>
+    <div>i am test module</div>
+</template>
