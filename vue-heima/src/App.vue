99fd90a6ba87059353f6bd4fd6b82dcd584c7f7d
codewater404 <1746559397@qq.com>
2023-09-24 12:27:56 +0800

    vue-heima: watch

--- vue-heima/src/App.vue
+++ vue-heima/src/App.vue
@@ -1,24 +1,77 @@
-<!-- computed -->
+<!-- watch函数 -->
+
 <script setup>
-import { ref } from 'vue'
-import { computed } from 'vue'
+import { ref, watch } from 'vue'
 
-const list = ref([1, 2, 3, 4, 5, 6, 7, 8])
+const count = ref(0)
+const name = ref('name')
+const changeName = () => {
+    name.value = 'changed name...'
+}
+const setCount = () => {
+    count.value++
+}
 
-const computedList = computed(() => {
-    return list.value.filter(item => item > 3)
+// 1.watch监听单个数据源；ref对象不需要加value
+// watch(count, (newVal, oldVal) => {
+//     console.log("count has changed: ", newVal, oldVal)
+// })
+
+// 2.监听多个数据源
+// watch(
+//     [count, name],
+//     (
+//         [newCount, newName],
+//         [oldCount, oldName]
+//     ) => {
+//         console.log("count or name has changed: ", [newCount, newName], [oldCount, oldName])
+//     })
+
+// 3.watch立即执行
+watch(count, () => {
+    console.log("count has changed....")
+}, {
+    immediated: true
 })
 
-setTimeout(() => {
-    list.value.push(9, 10)
-}, 3000)
+// 4. watch:开启深层监听(递归来的，所以性能有损耗)
+// 默认机制：通过watch监听的ref对象默认是浅层侦听的，直接修改嵌套的对象属性不会触发回调执行，需要开启deep选项
+const obj = ref({
+    count: 1,
+    age: 18
+})
+
+const changeObjCount = () => {
+    obj.value.count++
+}
+
+const changeObjAge = () => {
+    obj.value.age++
+}
+
+// watch(obj, () => {
+//     console.log("all obj's attributes have changed...")
+// }, {
+//     deep: true
+// })
+
+// 5.精确监听
+watch(
+    () => obj.value.age,
+    () => console.log("obj's age attribute has changed....")
+)
 </script>
 
 <template>
     <div>
-        原始响应式数据：{{ list }}
+        <button @click="setCount">{{ count }}</button>
     </div>
     <div>
-        计算属性数据：{{ computedList }}
+        <button @click="changeName">{{ name }}</button>
+    </div>
+    <p>深层监听</p>
+    <div>
+        <button @click="changeObjCount">count:{{ obj.count }}</button>
+        <button @click="changeObjAge">age:{{ obj.age }}</button>
     </div>
 </template>
---
+++ vue-heima/src/AppExample_3.vue
@@ -0,0 +1,30 @@
+<!-- computed：
+1.计算属性中不应该有“副作用”
+    比如异步请求/修改dom
+2.避免直接修改计算属性的值
+    计算属性应该是只读的
+
+-->
+<script setup>
+import { ref } from 'vue'
+import { computed } from 'vue'
+
+const list = ref([1, 2, 3, 4, 5, 6, 7, 8])
+
+const computedList = computed(() => {
+    return list.value.filter(item => item > 3)
+})
+
+setTimeout(() => {
+    list.value.push(9, 10)
+}, 3000)
+</script>
+
+<template>
+    <div>
+        原始响应式数据：{{ list }}
+    </div>
+    <div>
+        计算属性数据：{{ computedList }}
+    </div>
+</template>
