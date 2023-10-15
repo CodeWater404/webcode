2452607daacc36ff8b4693209404fe535994d9f2
codewater404 <1746559397@qq.com>
2023-10-15 22:21:52 +0800

    vue-heima-rabbit: custom  elementplus's theme color

--- vue-heima-rabbit/vite.config.js
+++ vue-heima-rabbit/vite.config.js
@@ -1,11 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
+import {
+  fileURLToPath,
+  URL
+} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 // 引入插件
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {
+  ElementPlusResolver
+} from 'unplugin-vue-components/resolvers'
 
 
 // https://vitejs.dev/config/
@@ -17,13 +24,28 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: "sass"
+        }),
+      ],
     }),
   ],
   resolve: {
     // 这个配置项可以让你对于@的导入可以直接跳转，也就是路径转换
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',
+        import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
