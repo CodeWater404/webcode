06662010ed46f6b07f53bc9b9f05fc3fbc0a1446
codewater404 <1746559397@qq.com>
2023-10-16 23:29:28 +0800

    vue-heima-rabbit: import static resource file

--- vue-heima-rabbit/src/main.js
+++ vue-heima-rabbit/src/main.js
@@ -1,5 +1,3 @@
-import './assets/main.css'
-
 import {
     createApp
 } from 'vue'
@@ -9,7 +7,8 @@ import {
 
 import App from './App.vue'
 import router from './router'
-
+// 引入初始化样式文件
+import '@/styles/common.scss'
 // 测试接口函数
 import {
     getCategory
