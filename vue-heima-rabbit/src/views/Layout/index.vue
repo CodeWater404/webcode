c286e349438beb8641b202c6e61cf30c71051c2b
codewater404 <1746559397@qq.com>
2023-10-18 22:19:35 +0800

    vue-heima-rabbit: a static layout page is set up

--- vue-heima-rabbit/src/views/Layout/index.vue
+++ vue-heima-rabbit/src/views/Layout/index.vue
@@ -1,5 +1,12 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
+
 <template>
-    <div>我是首页</div>
-    <!-- 这里再加载一次路由是因为有子路由 -->
+    <LayoutNav />
+    <LayoutHeader />
     <RouterView />
+    <LayoutFooter />
 </template>
