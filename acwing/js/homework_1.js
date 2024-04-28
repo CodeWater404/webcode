fb483c20b52e656d12c2b3974111725464c65d2b
codewater404 <1746559397@qq.com>
2024-04-28 23:51:23 +0800

    web-acwing: add comment for homework_1

--- acwing/js/homework_1.js
+++ acwing/js/homework_1.js
@@ -1,3 +1,20 @@
+/*
+输入两个整数，求这两个整数的和是多少。
+
+输入格式
+输入两个整数A,B
+，用空格隔开
+
+输出格式
+输出一个整数，表示这两个数的和
+
+数据范围
+0≤A,B≤108
+输入样例
+3 4
+输出样例
+7
+*/
 let buf = "";
 
 process.stdin.on("readable", function () {
@@ -5,11 +22,11 @@ process.stdin.on("readable", function () {
     if (chunk) {
         buf += chunk.toString();
     }
-})
+});
 
 process.stdin.on("end", function () {
     let [a, b] = buf.split(' ').map(x => {
         return parseInt(x);
     });
     console.log(a + b);
-})
+});
